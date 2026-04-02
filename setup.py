6fa19962ad8a0d39eab629746ca70e3c215ae1cf
Ibrahim Ahmed Galib <galib@Mac-mini.local>
2026-04-02 07:43:45 +0600

    setup file updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages 
+
+setup(
+    name='medical_chatbot',
+    version='0.1',
+    author="Ibrahim Ahmed Galib",
+    author_email="ibrahimgalib00@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
